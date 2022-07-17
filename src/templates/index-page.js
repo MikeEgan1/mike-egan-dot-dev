99dd80ced120b78f3c34ecbef44392522f524af3
Mike Egan <phegan@gmail.com>
2022-07-17 13:59:14 -0400

    Added back latest articles header

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -35,6 +35,9 @@ export const IndexPageTemplate = ({
                     </div>
                   </div>
                   <div className="column is-12">
+                    <h3 className="has-text-weight-semibold is-size-2">
+                      Latest articles
+                    </h3>
                     <BlogRoll />
                     <div className="column is-12 has-text-centered">
                       <Link className="btn" to="/blog">
